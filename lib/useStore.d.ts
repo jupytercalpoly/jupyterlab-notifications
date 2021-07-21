52253b710e035cccbadc47cfb059532cec73a02b
Andrii <ieroshenkoa@gmail.com>
2021-07-21 08:51:26 -0700

    delete/untrack nonused files

--- lib/useStore.d.ts
+++
@@ -1,4 +0,0 @@
-export declare let store: any;
-export declare function setStore(val: object | ((store: any) => object)): void;
-export declare function getStore(): any;
-export default function useStore(): any[];
