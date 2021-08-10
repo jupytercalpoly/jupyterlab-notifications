96fff9223fd75fd2ed282522cedaefef23d2fded
andrii-i <ieroshenkoa@gmail.com>
2021-08-10 10:51:06 -0700

    add return type

--- src/snackbar.tsx
+++ src/snackbar.tsx
@@ -4,7 +4,7 @@ import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 
-export default function SimpleSnackbar() {
+export default function SimpleSnackbar(): JSX.Element {
   const [open, setOpen] = React.useState(false);
   const handleClose = () => {
     setOpen(false);
