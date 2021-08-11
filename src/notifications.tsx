d23c1b3c6b964d2be5b5e296447f1819b318d7cb
andrii-i <ieroshenkoa@gmail.com>
2021-08-11 14:21:42 -0700

    remove uneeded export

--- src/notifications.tsx
+++ src/notifications.tsx
@@ -3,7 +3,7 @@ import { getStore, setStore } from "./useStore";
 import React from "react";
 import ImgMediaCard from "./card";
 import { ReactWidget } from "@jupyterlab/apputils";
-import { INotificationResponse, INotificationStoreObject } from "./index";
+import { INotificationStoreObject } from "./index";
 
 export function systemNotification(notification: any) {
   if (Notification.permission !== "granted") {
