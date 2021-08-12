c144ee9ae2545c62cfb275f665d9b5fe391c10f0
andrii-i <ieroshenkoa@gmail.com>
2021-08-12 11:50:16 -0700

    Add accordion
    Co-authored-by: Harshit Mittal <hxmittal@users.noreply.github.com>

--- src/notifications.tsx
+++ src/notifications.tsx
@@ -4,6 +4,27 @@ import React from "react";
 import ImgMediaCard from "./card";
 import { ReactWidget } from "@jupyterlab/apputils";
 import { INotificationStoreObject } from "./index";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Box from "@material-ui/core/Box";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: "100%",
+  },
+  expanded: {},
+  content: {
+    "&$expanded": {
+      marginBottom: 0,
+    },
+  },
+}));
 
 export function systemNotification(notification: any) {
   if (Notification.permission !== "granted") {
@@ -29,6 +50,8 @@ export function systemNotification(notification: any) {
 export function NotificationCenter(props: any) {
   const [store, setStore] = useStore();
 
+  const classes = useStyles();
+
   let handleClick = () => {
     console.log(store);
     setStore({ ...store });
@@ -37,22 +60,55 @@ export function NotificationCenter(props: any) {
     // ydoc.getArray('notif').insert(0, [6,7,8]);
     // console.log(ydoc.getArray('notif').toArray(), "print2");
   };
+  
   return (
     <div>
       {store.originStore.map((obj: any): any => (
-        <div >
-          {obj.origin}
-          {obj.notifications.map((notif: any) => (
-            <ImgMediaCard
-              title={notif.title}
-              body={notif.body}
-              id={notif.notificationId}
-              key={notif.notificationId}
-              origin={notif.origin}
+        <div className={classes.root}>
+          {/* bool defaultExpanded below controls default state of accordion */}
+          <Accordion defaultExpanded={true} elevation={0}>
+            <AccordionSummary
+              classes={{ content: classes.content, expanded: classes.expanded }}
+              expandIcon={<ExpandMoreIcon />}
+              aria-label="Expand"
+              aria-controls="additional-actions1-content"
+              id="additional-actions1-header"
             >
-              {/* control={ */}
-            </ImgMediaCard>
-          ))}
+              <FormControlLabel
+                aria-label="Acknowledge"
+                onClick={(event) => event.stopPropagation()}
+                onFocus={(event) => event.stopPropagation()}
+                control={
+                  <Checkbox
+                    checked={false}
+                    //id={task.id.toString()}
+                    //onChange={toggleTask}
+                  />
+                }
+                label={
+                  <div>
+                    {obj.origin} 
+                  </div>
+                }
+              />
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography color="textSecondary" component={"span"}>
+                <Box pl={4}>
+                  {obj.notifications.map((notif: any) => (
+                    <ImgMediaCard
+                      title={notif.title}
+                      body={notif.body}
+                      id={notif.notificationId}
+                      origin={notif.origin}
+                    >
+                      {/* control={ */}
+                    </ImgMediaCard>
+                  ))}
+                </Box>
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </div>
       ))}
       <button onClick={() => handleClick()}>Activat Lasers</button>
